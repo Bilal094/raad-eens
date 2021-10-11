5bbc88cf23c7383e00b2679a05deda7142eaed2a
Bilal094 <99066318@mydavinci.nl>
2021-10-11 13:11:39 +0200

    Game afegmaakt

--- raadeens.py
+++ raadeens.py
@@ -1,47 +1,49 @@
+# Variabelen
 Punt = 0
 Ronde = 1
 Spel = True
-from random import randint, randrange
-x = randrange(1,1000)
+Poging = 10
 
+# De game
 while Spel == True:
-    
-    for y in range(3):
-        print(x)
-        Question = int(input('Raad een getal tussen de 1 en 1000: '))
-        if Question == x:
-            print('Je hebt het geraden!')
-            Punt = Punt + 1
-            Ronde = Ronde + 1
-            print('Je hebt '+ str(Punt) +' punten.')
-            print('Je zit nu in ronde ' + str(Ronde) +'.')
-            break
-        elif Question >= + 50:
-            print('Je zit hoger dan het getal en er zitten max. 50 getallen ernaast, je bent warm')
-        elif Question <= - 50:
-            print('Je zit lager dan het getal en er zitten max. 50 getallen ervoor, je bent warm')
-        elif Question > x and Question < x + 20:
-            print('Je zit hoger dan het getal en er zitten max. 20 getallen ernaast, je bent heel warm')
-        elif Question < x and Question > x + 20:
-            print('Je zit lager dan het getal en er zitten max. 20 getallen ervoor, je bent heel warm')
-        elif Question > x:
-            print('Je zit hoger dan het random getal, je bent koud')
-        elif Question < x:
-            print('Je zit onder het random getal, je bent koud')
-    if Question != x:
-        print('Het getal was ' + str(x) + '!')
-        Hervatten1 = input('Wil je nog doorgaan? J/N ')
-
-    if Hervatten1 == 'j' or 'J':
-        print('Het spel wordt hervat')
-    if Hervatten == 'n' or 'N':
-        print('Je hebt gekozen om te stoppen. Je was bij ronde ' + str(Ronde) + ' en je had ' + str(Punt) + ' punten.')
-        Spel = False
-    else:
-        print('Type alleen \'J\/j\' of \'N\/n\' in! Je was bij ronde '+ str(Ronde))
-        Spel = False
-    
-    
-
-
-
+        from random import randint, randrange
+        Getal = randint(1,1000)
+        Raden = input('Wil je een getal raden? J/N ')
+        if Raden == 'J' or 'j':
+            print('Ronde '+ str(Ronde))
+            for x in range(10):
+                Gokken = int(input('Raad het geheime getal: '))
+                if Gokken == Getal:
+                    print('Je hebt het geraden!')
+                    Ronde += 1
+                    Punt += 1
+                    print('Je zit nu in ronde '+ str(Ronde))
+                    print('Je hebt '+ str(Punt) +' punten')
+                    break
+                if Gokken > Getal:
+                    print('Je zit hoger dan het geheime getal')
+                if Gokken < Getal:
+                    print('Je zit lager dan het geheime getal')
+                if Gokken > Getal and Gokken < Getal + 50:
+                    print('Je zit hoger dan het geheime getal, max. 50 getallen ernaast.')
+                if Gokken < Getal and Gokken > Getal - 50:
+                    print('Je zit lager dan het geheime getal, max. 50 getallen eronder.')
+                if Gokken >= Getal and Gokken <= Getal + 20:
+                    print('Je zit hoger dan het geheime getal en je bent heel warm, max. 20 getallen ernaast.')
+                if Gokken <= Getal and Gokken >= Getal - 20:
+                    print('Je zit lager dan het geheime getal en je bent heel warm, max. 20 getallen ervoor.')
+                if Gokken != Getal:
+                    Poging -= 1
+                    print('Je hebt nog '+ str(Poging) +' pogingen')
+            if Poging == 0:
+                print('Je hebt geen pogingen meer! Het juiste getal is '+ str(Getal))
+                Spel = False
+            if Ronde == 20:
+                print('Je hebt alles goed geraden! Je kwam bij ronde '+ str(Ronde) +' en je hebt '+ str(Punt) +' punten gehaald.')
+                Spel = False
+        elif Raden == 'N' or 'n':
+            print('Spel gestopt')
+            Spel = False
+        else:
+            print('Type alleen \'J\j\' of \'N/n\' in!')
+            Spel = False
